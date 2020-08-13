b3f5c51de582c3dd323567c61061c1dbdbf5579a
ajaybadole369 <ajaybadole654@gmail.com>
2020-08-13 09:47:59 +0530

    sum of arr in comment

--- array.py
+++ array.py
@@ -3,3 +3,10 @@ arr=array('i',[12,21,21,112,21,22,211])
 arr.insert(2,1001) # insert by indexing
 for i in arr:
     print(i)
+
+
+# arr1=[1,2,3,4,5,6,7,8]
+# sum=0
+# for i in arr1:
+#     sum=sum+i
+# print(sum)
